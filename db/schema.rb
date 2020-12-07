2393f635f9cba3232c8bc5a8649d52f412abf7bb
EcstasyH <wuhao164@mails.ucas.ac.cn>
2020-12-07 23:47:32 +0800

    add a boolean field to message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_124629) do
+ActiveRecord::Schema.define(version: 2020_12_07_154613) do
 
   create_table "messages", force: :cascade do |t|
     t.string "body"
+    t.boolean "from_bot"
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
